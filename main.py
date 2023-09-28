790ce2a3a33d32914f4d5a849c50ece6671fc6d2
Aminat Ajagbe <ajagbeaminat95@gmail.com>
2023-09-28 16:27:18 +0100

    Add upload_file function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
 
 app = FastAPI()
 
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "Welcome A3!!"}
+
+
+@app.post("/api/upload/")
+async def upload_file(file: UploadFile):
+    return {"filename": file.filename}
